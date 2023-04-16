01af07eeef1f909f7afedfe77038bb6d2e290883
Nargiza14 <ruzieva.aya@icloud.com>
2023-04-16 22:02:11 +0600

    feat: add trim to dream team file

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,38 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(members) {
-  if(!Array.isArray(members)){
-    return false;
-  }
-
-  let elements=0;
-  let isUpperCase;
-
-  members.forEach(value => {
-    if(typeof value=='string'){
-      elements++;
-    }else{
-      return false;
-    }
-  })
-
-  if (elements<0){
-    return false;
-  }
-
-  let realMembers = members.map(value=>{
-    if(typeof (value)=='string'){
-      for (let i=0; i<value.length; i++){
-        if(value[i] != ''){
-          return value[i].toUpperCase();
-        }
+  if (Array.isArray(members)){
+    let name = "";
+    let realMembers=[];
+    for(let i=0; i< members.length; i++){
+      if(typeof members[i] === 'string'){
+        realMembers.push(members[i].trim().toUpperCase());
       }
     }
-  })
-
-  realMembers = realMembers.sort().join('');
-  
-  return realMembers;
+    realMembers = realMembers.sort();
+    for(let j=0; j<realMembers.length; j++){
+      let elem= realMembers[j];
+      if (typeof elem === 'string'){
+        name += elem[0];
+      }
+    }
+    return name;
+  }
+  return false;
 }
 
 
