753d0b1ac006531a1c986447303a1a6c376083dc
Nargiza14 <ruzieva.aya@icloud.com>
2023-04-18 00:54:48 +0600

    feat: add vigenere function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect=true){
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!message || !key){
+      throw new Error('Incorrect arguments!');
+    }
+    message= message.toUpperCase();
+    key=key.toUpperCase();
+    let encryptedMessage= '';
+    let index=0;
+
+    for(let i=0; i<message.length; i++){
+      const charCode=message.charCodeAt(i);
+      if(charCode<65 || charCode>90){
+        encryptedMessage += message[i];
+        continue;
+      }
+      const code = key.charCodeAt(index)-65;
+      const encryptLetter = ((charCode-65+code) % 26)+65;
+      encryptedMessage += String.fromCharCode(encryptLetter);
+      index++;
+
+      if(index===key.length){
+        index=0;
+      }
+    }
+    return this.isDirect ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if(!encryptedMessage || !key){
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage= encryptedMessage.toUpperCase();
+    key=key.toUpperCase();
+    let decryptedMessage= '';
+    let index=0;
+
+    for(let i=0; i<encryptedMessage.length; i++){
+      const charCode=encryptedMessage.charCodeAt(i);
+      if(charCode<65 || charCode>90){
+        decryptedMessage += encryptedMessage[i];
+        continue;
+      }
+      const code = key.charCodeAt(index)-65;
+      const decryptLetter = ((charCode-65-code+26)%26)+65;
+      decryptedMessage += String.fromCharCode(decryptLetter);
+      index++;
+
+      if(index===key.length){
+        index=0;
+      }
+    }
+    return this.isDirect ? decryptedMessage : decryptedMessage.split('').reverse().join('');
   }
 }
 
